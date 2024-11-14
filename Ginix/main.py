7133e1f3fc7124a9877e1db77076d804cbdf6c06
voencev <95476667+voencev@users.noreply.github.com>
2024-11-14 14:41:24 +0300

    Add files via upload

--- Ginix/main.py
+++ Ginix/main.py
@@ -1,4 +1,15 @@
 from machine import deepsleep
+
+import time
+UTC_OFFSET = 3
+
+import os
+import sys
+
+import network
+wifi = network.WLAN(network.STA_IF)
+
 with open('/bin/shell.py') as shell:
     exec(shell.read())
+    
 deepsleep()
